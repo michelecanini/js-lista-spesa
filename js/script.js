c52c4613df5e0be9af62a207c3621f2822817291
michelecanini <studio.michelecanini@gmail.com>
2023-05-24 18:08:00 +0200

    [UPDATE] cancel c-log end item undefined

--- js/script.js
+++ js/script.js
@@ -18,35 +18,29 @@ let shopping_list = [
     'pasta',
     'pizza'
 ]
-console.log(shopping_list)
 
 //COSTANTE DEL TAG HTML E DELL'ARRAY
 const container_list = document.getElementById('shopping_list')
-console.log(container_list)
 
 // VARIABILE = 0 CONTATORE DELL'ARRAY
 let counter = 0;
-console.log(counter)
 
 /* 
 PARTE 2 LOGICA DEL PROGRAMMA
 */
 
 // CREAZIONE DEL CICLO WHILE PER SCORRERE GLI ELEMENTI DELLA LISTA
-while (counter < shopping_list.length) {
+while (counter < shopping_list.length - 1) {
     counter++;
 
     // VARIABILE DELL'ITEM
     let item = shopping_list[counter]
-    console.log(item)
 
     // VARIABILE CONTENITORE HTML DI 'LI'
     let container_item = document.createElement('li')
-    console.log(container_item)
 
     // INSERIMENTO DELL'ITEM ALL'INTERNO DEL CONTAINER
     container_item.innerText = item;
-    console.log(container_item)
 
     // INSERIMENTO DELLA LISTA
     container_list.appendChild(container_item)
