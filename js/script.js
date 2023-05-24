5931f405f40b2834c57cf628efe8d52f8c6b4a46
michelecanini <studio.michelecanini@gmail.com>
2023-05-24 16:54:51 +0200

    [UPDATE] create a cicle while

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ let shopping_list = [
     'pasta',
     'pizza'
 ]
-
 console.log(shopping_list)
 
 //COSTANTE DEL TAG HTML E DELL'ARRAY
@@ -26,4 +25,16 @@ const container_list = document.getElementById('shopping_list')
 console.log(container_list)
 
 // VARIABILE = 0 CONTATORE DELL'ARRAY
-let counter = 0
+let counter = 0;
+console.log(counter)
+
+/* 
+PARTE 2 LOGICA DEL PROGRAMMA
+*/
+
+// CREAZIONE DEL CICLO WHILE PER SCORRERE GLI ELEMENTI DELLA LISTA
+while (counter < shopping_list.length) {
+
+    // VARIABILI INTERNE AL CICLO
+
+}
